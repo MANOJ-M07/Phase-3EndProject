cdfde56dd9caded8c81fb8f4de0d991beee9646d
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-09-07 15:59:06 +0530

    initial commit

---
+++ PhaseEndProject/PhaseEndProject/Data/EmpsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PhaseEndProject.Models;
+
+namespace PhaseEndProject.Data
+{
+    public class EmpsDbContext : DbContext
+    {
+        public EmpsDbContext (DbContextOptions<EmpsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PhaseEndProject.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<PhaseEndProject.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
